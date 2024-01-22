7028c6d259b29100697cd7dd8c01cc3e2e834de2
Juan Ignacio Garcia <juanigarciadev@gmail.com>
2024-01-21 23:00:55 -0300

    Add: First slash command

--- src/index.js
+++ src/index.js
@@ -14,13 +14,11 @@ client.on('ready', (c) => {
     console.log(`✅ ${c.user.username} is online.`)
 })
 
-client.on('messageCreate', (message) => {
-    if (message.author.bot) {
-        return;
-    }
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
 
-    if (message.content === 'zolution') {
-        message.reply('yo')
+    if (interaction.commandName === 'ping') {
+        interaction.reply('pong');
     }
 })
 
---
+++ src/register-commands.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const { REST, Routes } = require("discord.js");
+
+const commands = [{ name: "ping", description: "Replies with pong" }];
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("Registering slash commands...");
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+    console.log("Slash commands registered successfully.");
+  } catch (error) {
+    console.log(`An error has ocurred: ${error}`);
+  }
+})();
